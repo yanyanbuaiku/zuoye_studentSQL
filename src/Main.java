8c8659cb11dafb9fd69069adc48688f1ab37b509
nihao <15838806586@163.com>
2023-11-30 15:06:27 +0800

    SQL驱动基本正常

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Connection a= SQLDriver.getSQLConnection();
+        List<Student> b= SQLDriver.searchStudent(a,"19","Age");
 
+
+
+
+        return;
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -3,6 +3,7 @@ import java.io.Serializable;
 /**
  * 支持序列化<p>
  * 提供get和set方法
+ *
  */
 public class Student implements Serializable {
     private String Student_ID; //学号
@@ -20,9 +21,77 @@ public class Student implements Serializable {
     private String StudentRoom = null; //宿舍
     //char 20位
 
-    public Student(String student_ID, String userName, String userPassword,
-                   String name, Boolean sex, int age, String phone_1,
-                   String phone_2, String studentRoom) {
+    public Student(){}
+    public Student(String student_ID, String name, Boolean sex, int age,
+                   String phone_1, String phone_2, String studentRoom) {
+        this.Student_ID = student_ID;
+        this.Name = name;
+        this.Sex = sex;
+        this.Age = age;
+        this.Phone_1 = phone_1;
+        this.Phone_2 = phone_2;
+        this.StudentRoom = studentRoom;
+    }
 
+
+    public String getStuAll() {
+        return Student_ID + Name + Sex + Age
+                + Phone_1 + Phone_2 + StudentRoom;
+    }
+
+    public String getStudent_ID() {
+        return Student_ID;
+    }
+
+    public void setStudent_ID(String student_ID) {
+        Student_ID = student_ID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Boolean getSex() {
+        return Sex;
+    }
+
+    public void setSex(Boolean sex) {
+        Sex = sex;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    public String getPhone_1() {
+        return Phone_1;
+    }
+
+    public void setPhone_1(String phone_1) {
+        Phone_1 = phone_1;
+    }
+
+    public String getPhone_2() {
+        return Phone_2;
+    }
+
+    public void setPhone_2(String phone_2) {
+        Phone_2 = phone_2;
+    }
+
+    public String getStudentRoom() {
+        return StudentRoom;
+    }
+
+    public void setStudentRoom(String studentRoom) {
+        StudentRoom = studentRoom;
     }
 }
